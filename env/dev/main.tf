9b426c4a9ef7deed007ebd706c11ca579dcaae03
hawk0722 <siroinekotokuroineko@gmail.com>
2023-09-15 09:14:34 +0900

    fix main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "s3" {
   bucket = "${var.project}-${var.env}-bucket-20230915"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "${var.project}-${var.env}-s3"
     Environment = var.env
   }
 }
